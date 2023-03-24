98c6e04e8d3ecfa31a440ff9327d058f48a4929d
Isak Larsson <isaklarsson98@hotmail.com>
2023-03-24 08:55:43 +0100

    image sizing

--- components/ChakraNextImage.tsx
+++ components/ChakraNextImage.tsx
@@ -2,7 +2,7 @@ import NextImage from 'next/image'
 import { chakra } from '@chakra-ui/react'
 
 const Image = chakra(NextImage, {
-    baseStyle: { maxH: 120, maxW: 120 },
+    baseStyle: { maxH: 150, maxW: 150 },
     shouldForwardProp: (prop) =>
         [
             'width',
